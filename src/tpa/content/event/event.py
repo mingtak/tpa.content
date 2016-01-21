811c703f5507fbba50d26a0e29cf55211ec215f5
andy fang <andy@mingtak.com.tw>
2016-01-21 03:33:39 +0000

    add event, moveContentToTop

---
+++ src/tpa/content/event/event.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# from Products.CMFPlone.utils import safe_unicode
+# from plone import api
+
+
+def moveContentToTop(item, event):
+    """ Moves Item to the top of its folder """
+    folder = item.getParentNode()
+    if hasattr(folder, 'moveObjectsToTop'):
+        folder.moveObjectsToTop(item.id)
